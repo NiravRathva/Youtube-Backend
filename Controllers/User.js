58ee8421c047fa53556fc30705888bd92ccbd2cb
NiravRathva <nsrathwa01@gmail.com>
2022-11-09 09:25:12 +0530

    "commit"

--- Controllers/User.js
+++ Controllers/User.js
@@ -50,7 +50,7 @@ export const getUser = async (req, res, next) => {
 export const subscribe = async (req, res, next) => {
   try {
     await User.findByIdAndUpdate(req.user.id, {
-      $push: { subscribedUsers: req.params.id },
+      $push: { subscribeduser: req.params.id },
     });
     await User.findByIdAndUpdate(req.params.id, {
       $inc: { subscribers: 1 }
